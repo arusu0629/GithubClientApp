499d046759d2374a40a6efe314925a594fd8087f
Toru_Nakandakari <arusu0629@gmail.com>
2020-02-24 13:34:50 +0900

    add initial project

---
+++ GithubClient/GithubClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubClient
+//
+//  Created by Toru Nakandakari on 2020/02/24.
+//  Copyright © 2020 仲村渠亨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
